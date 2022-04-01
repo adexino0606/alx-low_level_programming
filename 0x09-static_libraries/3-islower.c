0cafb7d877adb0eb2561ca9dd2779535654cdc0a
adexino0606 <eazi0606@gmail.com>
2022-04-01 15:26:06 -0700

    libmy.a, main.h

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,20 +1,13 @@
 #include "main.h"
-
 /**
- * _islower - checks for lowercase
- * @c: c is an ascii character
- *
- * Return: 1 if lowercase
- */
-
+*_islower - controls if a character is in lowercase
+*@c: character to be verified
+*Return: return 0 or 1
+*/
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if (c >= 'a' && c <= 'z')
+return (1);
+
+return (0);
 }
