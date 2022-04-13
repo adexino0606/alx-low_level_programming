84299ee577b0e6a43a9bbeb27272e2499cd1a91f
adexino0606 <eazi0606@gmail.com>
2022-04-12 23:21:48 -0700

    pointers

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+#Function pointers
+project done for Full Stack Software Engineering studies at Alx School. It aims to learn about function pointers in C language.
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
